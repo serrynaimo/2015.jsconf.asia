9329ed1072f78b5df9e1fc30cc41ddc7e0c033db
Thomas Gorissen <mail@serrynaimo.com>
2015-04-09 22:10:45 +0800

    update og

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 	<?php } ?>
 	<meta property="og:title" content="JSConf.Asia Singapore 2015"/>
 	<meta property="og:type" content="website"/>
-	<meta property="og:url" content="http://jsconf.asia/"/>
+	<meta property="og:url" content="http://2015.jsconf.asia/"/>
 	<meta property="og:image" content="http://jsconf.asia/assets/og_jsconfasia.jpg"/>
 	<meta property="og:site_name" content="JSConf.Asia"/>
 	<meta property="og:description" content='"It‘s a bit like TED just for web devs" Spread the word. Follow us on Twitter @jsconfasia'/>
