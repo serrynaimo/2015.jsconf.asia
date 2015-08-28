5b129e98f6b388db88191f9d952f6de270f04ac6
Thomas Gorissen <serrynaimo@me.com>
2015-08-28 15:24:06 +0900

    bugfix

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
       ga('create', 'UA-31025490-8', 'auto');
       ga('send', 'pageview');
 
-   CountDownTimer('08/28/2015 3:0 PM', 'countdown');
+   CountDownTimer('Fri 28 Aug 2015 15:00:00 +0800', 'countdown');
 
     function CountDownTimer(dt, id)
     {
