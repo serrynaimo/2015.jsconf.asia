d50182505f1dd5e5ea257dc23e48c97a25a6bfec
Thomas Gorissen <mail@serrynaimo.com>
2015-10-24 14:12:06 +0800

    comma

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
       <p>
         <br/>
         What is <i>this</i>, you may ask? Well, JSConf.Asia brings Southeast Asia's open web developer community together to exchange, to learn and entertain.
-        We showcase and teach the abilities of the web platform and inspire the next generation of tools, methods and solutions.
+        We showcase and teach the abilities of the web platform, inspiring the next generation of tools, methods and solutions.
         Come for the many amazing talks, exchange and collaborate at our coffee bar and make new friends over afternoon drinks.
         As with JavaScript, you've to step in, take a break and find out yourself what <i>this</i> is. All we want to share ahead of time is:<br/><br/>
         <i>It's a bit like TED, just for web devs.</i>
